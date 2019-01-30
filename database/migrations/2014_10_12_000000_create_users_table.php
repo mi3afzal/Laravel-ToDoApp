507cb29480ffb663969b8ee5ee3c3a6fe7956149
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2019-01-30 15:47:15 +0500

    Update user registration system to upload user image.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-			$table->string('userimage');
+			$table->string('userimage')->nullable();
         	$table->string('api_key')->nullable()->unique();
             $table->rememberToken();
             $table->timestamps();
